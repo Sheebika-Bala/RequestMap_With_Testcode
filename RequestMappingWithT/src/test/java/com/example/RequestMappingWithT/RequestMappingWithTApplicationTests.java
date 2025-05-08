8c47da71de33c6bd9daa142caec0ff39d0e7a611
Sheebika-Bala <128901514+Sheebika-Bala@users.noreply.github.com>
2025-05-08 13:13:59 +0530

    Add files via upload

---
+++ RequestMappingWithT/src/test/java/com/example/RequestMappingWithT/RequestMappingWithTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RequestMappingWithT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestMappingWithTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
